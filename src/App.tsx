e32bbbb335e7ba60a023260a48543fad4d14fb22
Hajialiyev-Frontend <hacieliyevahmed@gmail.com>
2026-05-05 18:22:23 +0400

    Header yeniləndi və Contact səhifəsi əlavə edildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import Contact from './pages/contact/contact';
 import About from './pages/about/About';
 import Shop from './pages/Shop/Shop'
 import Home from './assets/Home/Home';
@@ -14,10 +13,9 @@ function App() {
         <Route path='/' element={<Home/>}/>
          <Route path="/about" element={<About />} />
          <Route path='/Shop' element={<Shop />} />
-         <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
   );
-}
+}   
 
 export default App;
